8675a8e895e3328055aa536fa649bddebdc676f0
11ipsita <61733847+11ipsita@users.noreply.github.com>
2021-06-17 23:39:35 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+
+include 'money.php';
+
+$idt = $_GET['id'];
+
+$deletequery = "DELETE FROM `transactionhistory` WHERE ID=$idt ";
+
+$query = mysqli_query($con, $deletequery);
+
+if($query){
+    ?>
+    <script>
+       alert("Deleted Successfully");
+    </script>
+    <?php
+}else{
+    ?>
+    <script>
+       alert("Not Deleted");
+    </script>
+    <?php
+}
+
+header('location:transferhistory.php');
+
+
+
+?>
